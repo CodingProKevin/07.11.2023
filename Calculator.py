b7c208185fa83140ff01dc0d45c1a3d8c178abac
CodingProKevin <kevin.zolnierczyk@icloud.com>
2023-11-10 20:30:52 +0000

    Update Calculator.py
    able to enter multiple numbers in the calculator but cannot perform all calculations correctly

--- Calculator.py
+++ Calculator.py
@@ -1,43 +1,76 @@
 def checkOP(string):
     global Opos 
     global OP
+    Opos = []
+    OP = []
     counter = False
 
     for i in range(len(question)):
         if question[i] == "+" or question[i] == "-" or question[i] == "/" or question[i] == "*":
             counter = True
-            Opos = i
-            OP = question[i]
-            return Opos
+            Opos.append(i)
+            OP.append(question[i])
             
     if counter == False:
         print("Error, There is no operator")
         exit()
+    return 
 
 question = input("Enter your query(+,-,/,*):\n")
 
 checkOP(question)
 
-num1 = str("")
-num2 = str("")
-for i in range(len(question)):
-    if question[i].isnumeric() == True and i < Opos:
-        num1 = num1 + question[i]
-    elif question[i].isnumeric() == True and i > Opos:
-        num2 = num2 + question[i]
+numbers = []
+numX = ""
+for i in range(len(OP)):
+    if i-1 < 0:
+        for x in range(len(question)):
+            if question[x].isnumeric() == True and x < Opos[i]:
+                numX = numX + question[x]
+        numbers.append(numX)
+        numX = ""
 
-num1 = int(num1)
-num2 = int(num2)
-if OP == "+":
-    total = num1 + num2
-if OP == "-":
-    total = num1 - num2
-if OP == "/":
-    try:
-        total = num1 / num2
-    except:
-        print("You cannot divide these 2 number together")
-if OP == "*":
-    total = num1 * num2
+    if i-1 >= 0 and i != len(OP):
+        for x in range(len(question)):
+            if question[x].isnumeric() == True and x < Opos[i] and x > Opos[i-1]:
+                numX = numX + question[x]
+        numbers.append(numX)
+        numX = ""
+
+    if i+1 >= len(OP):
+        for x in range(len(question)):
+            if question[x].isnumeric() == True and x > Opos[i]:
+                numX = numX + question[x]
+        numbers.append(numX)
+        numX = ""
+
+print(numbers)
+
+
+
+total = 0
+for i in range(len(OP)):
+    if OP[i] == "/" and total == 0:
+         total = int(numbers[i]) / int(numbers[i+1])
+    elif OP[i] == "/":
+        total = total / int(numbers[i])
+
+for i in range(len(OP)):
+    if OP[i] == "*" and total == 0:
+         total = int(numbers[i]) * int(numbers[i+1])
+    elif OP[i] == "*":
+        total = total * int(numbers[i])
+
+for i in range(len(OP)):
+    if OP[i] == "+" and total == 0:
+         total = int(numbers[i]) + int(numbers[i+1])
+    elif OP[i] == "+":
+        total = total + int(numbers[i])
+        
+for i in range(len(OP)):
+    if OP[i] == "-" and total == 0:
+         total = int(numbers[i]) - int(numbers[i+1])
+    elif OP[i] == "-":
+        total = total - int(numbers[i])
 
 print(total)
