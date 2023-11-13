8df695cd3357ee89a25142107cd8475b9ab139f0
CodingProKevin <kevin.zolnierczyk@icloud.com>
2023-11-13 22:10:30 +0000

    Update Calculator.py
    coded the program so that it is able to now solve equiations,

--- Calculator.py
+++ Calculator.py
@@ -15,62 +15,65 @@ def checkOP(string):
         print("Error, There is no operator")
         exit()
     return 
+while True:
+    question = input("Enter your query(+,-,/,*):\n")
 
-question = input("Enter your query(+,-,/,*):\n")
+    if question == "exit" or question == "q":
+        exit()
 
-checkOP(question)
+    checkOP(question)
 
-numbers = []
-numX = ""
-for i in range(len(OP)):
-    if i-1 < 0:
-        for x in range(len(question)):
-            if question[x].isnumeric() == True and x < Opos[i]:
-                numX = numX + question[x]
-        numbers.append(numX)
-        numX = ""
+    numbers = []
+    numX = ""
+    for i in range(len(OP)):
+        if i-1 < 0:
+            for x in range(len(question)):
+                if question[x].isnumeric() == True and x < Opos[i]:
+                    numX = numX + question[x]
+            numbers.append(numX)
+            numX = ""
 
-    if i-1 >= 0 and i != len(OP):
-        for x in range(len(question)):
-            if question[x].isnumeric() == True and x < Opos[i] and x > Opos[i-1]:
-                numX = numX + question[x]
-        numbers.append(numX)
-        numX = ""
+        if i-1 >= 0 and i != len(OP):
+            for x in range(len(question)):
+                if question[x].isnumeric() == True and x < Opos[i] and x > Opos[i-1]:
+                    numX = numX + question[x]
+            numbers.append(numX)
+            numX = ""
 
-    if i+1 >= len(OP):
-        for x in range(len(question)):
-            if question[x].isnumeric() == True and x > Opos[i]:
-                numX = numX + question[x]
-        numbers.append(numX)
-        numX = ""
-
-print(numbers)
-
-
-
-total = 0
-for i in range(len(OP)):
-    if OP[i] == "/" and total == 0:
-         total = int(numbers[i]) / int(numbers[i+1])
-    elif OP[i] == "/":
-        total = total / int(numbers[i])
-
-for i in range(len(OP)):
-    if OP[i] == "*" and total == 0:
-         total = int(numbers[i]) * int(numbers[i+1])
-    elif OP[i] == "*":
-        total = total * int(numbers[i])
-
-for i in range(len(OP)):
-    if OP[i] == "+" and total == 0:
-         total = int(numbers[i]) + int(numbers[i+1])
-    elif OP[i] == "+":
-        total = total + int(numbers[i])
-        
-for i in range(len(OP)):
-    if OP[i] == "-" and total == 0:
-         total = int(numbers[i]) - int(numbers[i+1])
-    elif OP[i] == "-":
-        total = total - int(numbers[i])
-
-print(total)
+        if i+1 >= len(OP):
+            for x in range(len(question)):
+                if question[x].isnumeric() == True and x > Opos[i]:
+                    numX = numX + question[x]
+            numbers.append(numX)
+            numX = ""
+            
+    while ('/' in OP):
+        for x,y in enumerate(OP):
+            if y == "/":
+                try:
+                    numbers[x] = int(numbers[x]) / int(numbers[x+1])
+                    numbers.remove(numbers[x+1])
+                    OP.remove(y)
+                    break
+                except:
+                    print("The was an error with dividing")
+                    exit()
+    while ('*' in OP):        
+        for x,y in enumerate(OP):
+            if y == "*":
+                numbers[x] = int(numbers[x]) * int(numbers[x+1])
+                numbers.remove(numbers[x+1])
+                OP.remove(y)
+    while ('+' in OP) or ('-' in OP):
+        for x,y in enumerate(OP):
+            if y == "+":
+                numbers[x] = int(numbers[x]) + int(numbers[x+1])
+                numbers.remove(numbers[x+1])
+                OP.remove(y)
+                break
+            if y == "-":
+                numbers[x] = int(numbers[x]) - int(numbers[x+1])
+                numbers.remove(numbers[x+1])
+                OP.remove(y)
+                break
+    print(numbers[0])
